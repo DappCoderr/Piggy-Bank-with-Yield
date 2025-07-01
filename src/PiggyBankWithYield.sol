00a540e3080647f1f1061577de219b3504341bb5
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2025-07-01 21:38:33 +0530

    update contract

--- src/PiggyBankWithYield.sol
+++ src/PiggyBankWithYield.sol
@@ -1,11 +1,10 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.20;
 
 import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
-contract PiggyBankWithYield is ReentrancyGuard{
-
+contract PiggyBankWithYield is ReentrancyGuard {
     event Deposit(address indexed user, uint256 amount);
     event Withdraw(address indexed user, uint256 amount);
 
@@ -16,53 +15,53 @@ contract PiggyBankWithYield is ReentrancyGuard{
 
     constructor() {}
 
-    mapping (address user => uint amount) public depositor;
-    mapping (address => bool) public has_deposited;
+    mapping(address user => uint256 amount) public depositor;
+    mapping(address => bool) public hasDeposited;
 
-    function deposit() external payable{
+    function deposit() external payable {
         require(msg.value > MINIMUM_DEPOSIT, "You must send some ether");
         depositor[msg.sender] += msg.value;
-        has_deposited[msg.sender] = true;
+        hasDeposited[msg.sender] = true;
         emit Deposit(msg.sender, msg.value);
     }
-    function withdraw(uint256 amount, address _to) external nonReentrant{
 
-        if(!has_deposited[msg.sender]){
+    function withdraw(uint256 amount, address _to) external nonReentrant {
+        if (!hasDeposited[msg.sender]) {
             revert PIGGYBANK__DID_NOT_JOINED();
         }
 
-        if(depositor[msg.sender] >= amount){
+        if (depositor[msg.sender] >= amount) {
             revert PIGGYBANK__INSUFFICIENT_BALANCE();
         }
 
         depositor[msg.sender] -= amount;
 
-        (bool success, ) = _to.call{value: amount}("");
+        (bool success,) = _to.call{value: amount}("");
         require(success, "Transfer failed");
 
         emit Withdraw(_to, amount);
 
         if (depositor[msg.sender] == 0) {
-            has_deposited[msg.sender] = false; // Reset if balance is zero
+            hasDeposited[msg.sender] = false; // Reset if balance is zero
         }
     }
 
-    function get_Balance() public view returns(uint256){
+    function getBalance() public view returns (uint256) {
         return depositor[msg.sender];
     }
 
-    function get_HasDeposited() public view returns(bool){
-        return has_deposited[msg.sender];
+    function getHasDeposited() public view returns (bool) {
+        return hasDeposited[msg.sender];
     }
 
-    function get_MinimumDeposit() public view returns(uint256){
+    function getMinimumDeposit() public pure returns (uint256) {
         return MINIMUM_DEPOSIT;
     }
 
     receive() external payable {
         require(msg.value > MINIMUM_DEPOSIT, "You must send some ether");
         depositor[msg.sender] += msg.value;
-        has_deposited[msg.sender] = true;
+        hasDeposited[msg.sender] = true;
         emit Deposit(msg.sender, msg.value);
     }
 }
